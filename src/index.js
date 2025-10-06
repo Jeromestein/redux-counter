ca5616b4d6d0e5d258bcb1b82b4dc32ce53921ca
Jerome <nietsemorej@gmail.com>
2025-10-06 10:31:56 -0700

    Wrap App component with Redux Provider and create a Redux store with a simple reducer for state management.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Counter from './Counter';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+function reducer() {
+  return {
+    count: 42
+  };
+}
+
+const store = createStore(reducer);
 
 const App = () => (
-  <div>
+  <Provider store={store}>
     <Counter />
-  </div>
+  </Provider>
 );
 
 render(<App />, document.getElementById('root'));
